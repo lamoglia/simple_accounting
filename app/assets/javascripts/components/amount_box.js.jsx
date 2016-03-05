a1be7fc24daed36a835949ce380ab3a04b572f5d
Fellipe Lamoglia <fellipelamoglia@gmail.com>
2016-03-04 22:40:37 -0300

    Includes footer and some styling.

--- app/assets/javascripts/components/amount_box.js.jsx
+++ app/assets/javascripts/components/amount_box.js.jsx
@@ -1,13 +1,13 @@
 var AmountBox = React.createClass({
   render: function() {
     var panel_class_name = "panel panel-" + this.props.type;
-    return <div className="col-md-4">
+    return <div className="col-sm-4 col-xs-12">
               <div className={panel_class_name}>
                 <div className="panel-heading">
                   {this.props.text}
                 </div>
                 <div className="panel-body">
-                  {amountFormat(this.props.amount)}
+                  <strong>{amountFormat(this.props.amount)}</strong>
                 </div>
               </div>
             </div>;
